c4989bbdb9eb505604f50a0e90338a71511a8243
jiangheqing.jhq <jiangheqing.jhq@alibaba-inc.com>
2025-09-23 14:52:00 +0800

    fix: Apache-2.0 License header year

--- src/main/java/io/agentscope/core/tool/ToolParam.java
+++ src/main/java/io/agentscope/core/tool/ToolParam.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 the original author or authors.
+ * Copyright 2024-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * You may not use this file except in compliance with the License.
